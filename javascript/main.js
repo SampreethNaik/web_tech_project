bef156571dfb820257ccbcfaf6e6529fce9fb85b
Sampreeth Naik <sampreethnaik@gmail.com>
2019-10-15 10:54:39 +0530

    created login with jav support

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,4 @@
+/* for dashboard */
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
@@ -11,3 +12,43 @@ function closeNav() {
   document.getElementById("main").style.marginLeft = "0";
   document.body.style.backgroundColor = "white";
 }
+
+/*dashboard script ends here*/
+
+/* For login page */
+
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onsubmit);
+
+function onsubmit(e){
+    e.preventDefault();
+    //console.log(nameInput.value);
+    if(nameInput.value === '' || emailInput.value === ''){
+        //alert('Please enter fields');
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter fields';
+
+        setTimeout(() =>msg.remove(), 3000);
+    }
+    else{
+        const li = document.createElement('Li');
+        li.appendChild(document.createTextNode(`
+        ${nameInput.value} : ${emailInput.value}`));
+        
+
+        userList.appendChild(li);
+
+        //Clear fields
+        nameInput.value = '';
+        emailInput.value= '';
+    }
+
+}
+
+/*Login page script ends here */
